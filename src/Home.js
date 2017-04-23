e2fe5eacde4ddcb2dfee0ca660230ec443ec766f
Vsevolod <vsevolod@localhost.localdomain>
2017-04-23 20:45:08 +0300

    added TODO text file

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import TicketsShow from './TicketsShow'
 import Navbar from './Navbar'
 import About from './About'
 import Footer from './Footer'
-import Slider from 'react-slick';
 import './index.css'
 
 export default class Home extends Component {
@@ -16,7 +15,6 @@ export default class Home extends Component {
       <div className="content">
           <TicketsShow url='http://localhost:3001/api/tickets' pollInterval={2000} />
       </div>
-
       <About />
       <Footer />
     </div>
